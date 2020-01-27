459552f14386fedcafae8338ca0864b118ae2711
Souparno Paul <souparnopaul@gmail.com>
2020-01-27 20:30:40 +0530

    Initial Commit

--- photoapp/forms.py
+++ photoapp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile, Post, Comment
 from django.contrib.auth.forms import UserCreationForm
+from django.forms.widgets import SelectDateWidget
 
 class UserForm(UserCreationForm):
     class Meta:
--- photoapp/models.py
+++ photoapp/models.py
@@ -21,9 +21,10 @@ class Profile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
 	if created:
 		Profile.objects.create(user=instance)
+	
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+	instance.profile.save()
 
 
 class Post(models.Model):
