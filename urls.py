884e2f2ad02712044c05ca47a256cc1c518b3de4
Theseregrets <rishitashaw49@gmail.com>
2021-01-29 13:24:22 +0530

    added user profile page and comment sec

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns= [
+	path('', views.index, name='index'),
+	path('signup/', views.signup, name='signup'),
+	path('signup/update', views.update_profile, name='update_profile'),
+	path('login/', views.login_view, name='login'),
+	path('signout/', views.logout_view, name='logout'),
+	path('create_post/', views.create_post, name='create_post'),
+	path('home/', views.home, name='home'),
+	path('home/<key>/confirm_delete', views.confirm_delete, name='post-confirm-delete'),
+	path('home/<key>/confirm_delete/deleted', views.delete_post, name='post-delete'),
+	path('home/<key>/', views.view_post, name='post-detail'),
+	path('home/<key>/update', views.update_post, name='post-update'),
+	path('home/<key>/create_comment', views.create_comment, name='create-comment'),
+
+	path('home/<key>/like_post', views.like_post, name='like-post'),
+	path('home/userprofile', views.userprofile, name='userprofile'),
+	]
---
+++ views.py
@@ -0,0 +1,223 @@
+import username as username
+from django.shortcuts import render,HttpResponse, HttpResponseRedirect, redirect, reverse
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+from .forms import UserForm,CreatePostForm,UpdatePostForm,CreateCommentForm, UpdateProfileForm
+from django.contrib.auth.models import User
+from .models import Profile,Post, Comment, Likes
+from django.core.files.storage import FileSystemStorage
+from django.utils import timezone
+from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
+
+
+
+def index(request):
+	return render(request,'photoapp/home.html')
+
+
+def signup(request):
+	if request.method=='POST':
+		form=UserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password=form.cleaned_data.get('password1')
+			return redirect('login')
+	else:
+		form=UserForm()
+	args={'form': form}
+	return render(request,'photoapp/signup.html',args)
+
+
+def login_view(request):
+	message='Log In'
+	if request.method=='POST':
+		_username=request.POST['username']
+		_password=request.POST['password']
+		user=authenticate(username=_username,password=_password)
+		if user is not None:
+			if user.is_active:
+				login(request,user)
+				return redirect('/home')
+			else:
+				message='Not Activated'
+		else:
+			message='Invalid Login'
+	context={'message':message}
+	return render(request,'photoapp/login.html',context)
+
+@login_required
+def logout_view(request):
+	logout(request)
+	return HttpResponseRedirect('/')
+
+@login_required
+def create_post(request):
+	if request.method=='POST':
+		form=CreatePostForm(request.POST,request.FILES)
+		if form.is_valid():
+			p=form.save(commit=False)
+			p.author=request.user
+			p.date_posted=timezone.now()
+			p.save()
+			#form.save()
+			
+			return redirect('/home')
+	else:
+		form=CreatePostForm()
+	
+
+	return render(request,'photoapp/postenter.html',{'form':form})
+
+def update_profile(request):
+	if request.method=='POST':
+		form=UpdateProfileForm(request.POST,request.FILES, instance=request.user.profile)
+		if form.is_valid():
+			form.save()
+
+			return redirect('/home')
+
+	else:
+		form=UpdateProfileForm()
+
+	return render(request,'photoapp/update_profile.html',{'form':form})
+
+
+def home(request):
+	users=User.objects.all()
+	print(users)
+	
+	pk=request.user
+	
+	following=[]
+	posts=Post.objects.order_by("-date_posted")
+	for i in users:
+		#users=User.objects.get(id=user_id)
+		#print(i.pk)
+		#print(i)
+		#print(i.profile.bio)
+		#print(i.profile.caption)
+		#print(i.profile.followers.all())
+		if pk in i.profile.followers.all():
+
+			following.append(i.pk)
+	#print(following)
+	post_list=posts
+	#print(post_list)
+	return render(request,'photoapp/postlist.html',{'posts':post_list})
+
+def confirm_delete(request,key):
+	posts=Post.objects.get(id=key)
+	return render(request,'photoapp/confirm_delete.html',{'posts':posts})
+
+def delete_post(request,key):
+	posts=Post.objects.get(id=key)
+	posts.delete()
+	return redirect('/home')
+
+
+def view_post(request,key):
+	posts=Post.objects.get(id=key)
+	comments=Comment.objects.filter(post=posts).order_by('-date')
+	liked=Likes.objects.filter(post=posts)
+	print(liked.count())
+	l=liked.count()
+	co=[]
+	lo=[]
+	for i in liked:
+		lo.append(i)
+
+
+	for i in comments:
+		#print(i.text)l
+		#print(type(i))
+		co.append(i)
+	#print(co)
+	print(co)
+	#print(posts.author)
+	#print(request.user)
+	if posts.author==request.user:
+		return render(request,'photoapp/viewpost2.html',{'posts':posts,'co':co, 'l':l, 'user':request.user, 'likes':lo})
+	else:
+		return render(request,'photoapp/viewpost.html',{'posts':posts,'co':co, 'l':l, 'user':request.user, 'likes':lo})
+
+def update_post(request,key):
+	posts=Post.objects.get(id=key)
+	if request.method=='POST':
+		form=UpdatePostForm(request.POST)
+		if form.is_valid():
+			#posts=form.save(commit=False)
+			#posts.author=request.user
+			posts.image=posts.image
+			posts.caption=form.cleaned_data['caption']
+			posts.location=form.cleaned_data['location']
+			posts.date_posted=timezone.now()
+			posts.save()
+
+			return redirect('/home')
+	else:
+		form=UpdatePostForm()
+
+	return render(request,'photoapp/update_post.html',{'form':form})
+
+def create_comment(request,key):
+	posts=Post.objects.get(id=key)
+	comments=Comment.objects.filter(post=posts)
+	comment_list=[]
+	print(posts)
+	print(comments)
+	if request.method=='POST':
+		form=CreateCommentForm(request.POST)
+		if form.is_valid():
+			p=form.save(commit=False)
+			p.author=request.user
+			p.post=posts
+			p.save()
+			next = request.POST.get('next',None)
+			#return HttpResponseRedirect(next)
+			#return redirect('/home/key/')
+			return redirect('/home/%s' % key )
+	else:
+		form=CreateCommentForm()
+	for i in comments:
+		print(i)
+
+	return render(request,'photoapp/create_comment.html',{'form':form})
+def like_post(request,key):
+	posts=Post.objects.get(id=key)
+	liked=Likes()
+	liked.post=posts
+	liked.users=request.user
+	liked.save()
+	return redirect('/home/%s' % key )
+
+
+def userprofile(request):
+	#'''userid= Profile.objects.all()
+	#biod=Profile.objects.all()
+	#imagep=Profile.objects.all()
+
+	#params: dict
+	#params={
+		#'userid': userid,
+		#'bio':biod,
+		#'image':imagep,
+		#'followers':followers'''
+	#}#
+
+	return render(request, 'userprofile.html')
+	
+
+
+
+
+
+
+
+
+
+
